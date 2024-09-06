c7a1f138010addcce74c160924195b262f2d5e43
fabiopasivla1 <fabiopasilva1@gmail.com>
2024-09-06 02:27:05 -0300

    feat: add createOpening handler  logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,9 +1,34 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/fabiopasilva1/gooportunities/schemas"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "POST Opening v1",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error validating opening: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	sendSuccess(ctx, "create-opening", opening)
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/fabiopasilva1/gooportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
